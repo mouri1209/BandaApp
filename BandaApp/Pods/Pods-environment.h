7e0c858f43ce21c05b7605a56baee9f537df2688
Asher Coelho <asher.coelho@gmail.com>
2013-08-27 23:57:53 -0300

    Commit

--- BandaApp/Pods/Pods-environment.h
+++ BandaApp/Pods/Pods-environment.h
@@ -13,9 +13,3 @@
 // Please contact the author.
 // Version: 1.5pre.
 
-// Reachability
-#define COCOAPODS_POD_AVAILABLE_Reachability
-#define COCOAPODS_VERSION_MAJOR_Reachability 3
-#define COCOAPODS_VERSION_MINOR_Reachability 0
-#define COCOAPODS_VERSION_PATCH_Reachability 0
-
